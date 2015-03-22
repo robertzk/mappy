77bf3142880aaffe17584656a782623d4f61fa38
RobertZK <technoguyrob@gmail.com>
2015-03-21 23:05:39 -0500

    check it does not accept one unnamed expression

--- tests/testthat/test-mappy.R
+++ tests/testthat/test-mappy.R
@@ -1,6 +1,10 @@
 context("mappy")
 
 describe("Setting shortcuts", {
+  test_that("it does not accept one unnamed expression", {
+    expect_error(mappy(test), "must be named")
+  })
+
   test_that("setting a shortcut is possible in the global environment", {
     mappy(hi = cat("Hello world"))
     expect_output(hi, "Hello world")
