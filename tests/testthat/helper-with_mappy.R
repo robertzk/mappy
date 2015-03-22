fc1d10b37b3eb5eb5b467e3cb00f0b5d1963c48f
RobertZK <technoguyrob@gmail.com>
2015-03-22 16:59:11 -0500

    unlink directory tempdir

--- tests/testthat/helper-with_mappy.R
+++ tests/testthat/helper-with_mappy.R
@@ -8,10 +8,11 @@ with_options <- function(options, expr) {
 
 with_mappy <- function(expr) {
   tempdir <- tempdir()
+  unlink(tempdir, FALSE, TRUE)
   with_options(list(mappy.file = file.path(tempdir, "mappy")), {
     memoise::forget(registry_dir)
     memoise::forget(registry_obj)
-    on.exit(unlink(tempdir, TRUE, TRUE))
+    on.exit(unlink(tempdir, FALSE, TRUE))
     eval.parent(substitute(expr))
   })
 }
