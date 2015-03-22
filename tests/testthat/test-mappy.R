8778b4ac6924528401f2bd1d7c5b3b2070dec044
RobertZK <technoguyrob@gmail.com>
2015-03-21 23:06:30 -0500

    check it does not allow duplication in expression names

--- tests/testthat/test-mappy.R
+++ tests/testthat/test-mappy.R
@@ -5,6 +5,14 @@ describe("Setting shortcuts", {
     expect_error(mappy(test), "must be named")
   })
 
+  test_that("it does not accept one named and one unnamed expression", {
+    expect_error(mappy(test, b = test), "must be named")
+  })
+
+  test_that("it does not allow duplication in expression names", {
+    expect_error(mappy(b = test, b = test), "must be unique")
+  })
+
   test_that("setting a shortcut is possible in the global environment", {
     mappy(hi = cat("Hello world"))
     expect_output(hi, "Hello world")
