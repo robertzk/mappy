c969a439a8f00fa91599ae5dbc0dfc4c84ec36b2
RobertZK <technoguyrob@gmail.com>
2015-03-22 19:10:42 -0500

    use explicit scoping for CRAN-compatible check

--- tests/testthat/test-registry.R
+++ tests/testthat/test-registry.R
@@ -3,56 +3,56 @@ library(testthatsomemore)
 
 describe("Adding to the registry", {
   test_that("it errors when a non-character is passed", {
-    expect_error(register(5, quote(cat("hi"))))
+    expect_error(mappy:::register(5, quote(cat("hi"))))
   })
 
   test_that("it does nothing when no expressions are passed", {
     with_mappy({
-      register(character(0), quote(cat("hi")))
-      expect_equal(length(registry_map()), 0)
+      mappy:::register(character(0), quote(cat("hi")))
+      expect_equal(length(mappy:::registry_map()), 0)
     })
   })
 
   test_that("it errors when a non-language object is passed for the expression", {
-    expect_error(register("foo", 5))
+    expect_error(mappy:::register("foo", 5))
   })
 
   test_that("it can register an expression", {
     with_mappy({
-      register("a", quote(cat("Hello world")))
-      expect_true(is.element("a", names(registry_map())))
+      mappy:::register("a", quote(cat("Hello world")))
+      expect_true(is.element("a", names(mappy:::registry_map())))
     })
   })
 
   test_that("it can register multiple expressions", {
     with_mappy({
-      register(c("a", "b"), list(quote(cat("Hello world")), quote(cat("Hi"))))
-      expect_true(is.element("a", names(registry_map())))
-      expect_true(is.element("b", names(registry_map())))
+      mappy:::register(c("a", "b"), list(quote(cat("Hello world")), quote(cat("Hi"))))
+      expect_true(is.element("a", names(mappy:::registry_map())))
+      expect_true(is.element("b", names(mappy:::registry_map())))
     })
   })
 })
 
 describe("Removing from the registry", {
   test_that("it errors when a non-character is passed", {
-    expect_error(deregister(5))
+    expect_error(mappy:::deregister(5))
   })
 
   test_that("it can remove from the registry", {
     with_mappy({
-      register("a", quote(cat("Hello world")))
-      deregister("a")
-      expect_false(is.element("a", names(registry_map())))
+      mappy:::register("a", quote(cat("Hello world")))
+      mappy:::deregister("a")
+      expect_false(is.element("a", names(mappy:::registry_map())))
     })
   })
 
   test_that("it can remove multiple objects from the registry", {
     with_mappy({
-      register("a", quote(cat("Hello world")))
-      register("b", quote(cat("Hello world")))
-      deregister(c("a", "b"))
-      expect_false(is.element("a", names(registry_map())))
-      expect_false(is.element("b", names(registry_map())))
+      mappy:::register("a", quote(cat("Hello world")))
+      mappy:::register("b", quote(cat("Hello world")))
+      mappy:::deregister(c("a", "b"))
+      expect_false(is.element("a", names(mappy:::registry_map())))
+      expect_false(is.element("b", names(mappy:::registry_map())))
     })
   })
 })
@@ -61,7 +61,7 @@ describe("Loading a registry", {
   test_that("it can load the registry into an environment", {
     with_mappy({
       env <- new.env()
-      register("a", quote(env$y <- "Hello world"))
+      mappy:::register("a", quote(env$y <- "Hello world"))
       load_registry(env)
       env$a
       expect_equal(env$y, "Hello world")
@@ -73,7 +73,7 @@ describe("Unloading a registry", {
   test_that("it can unload the registry from an environment", {
     with_mappy({
       env <- new.env()
-      register("a", quote(cat("Hello world")))
+      mappy:::register("a", quote(cat("Hello world")))
       load_registry(env)
       unload_registry(env)
       expect_null(env$a)
